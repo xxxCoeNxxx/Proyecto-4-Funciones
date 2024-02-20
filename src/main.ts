d1371d5b9ba58c0ce7bae6cadbbcc07e28989ea6
xxxCoeNxxx <pedrocela1992@gmail.com>
2024-02-20 16:39:00 +0100

    Búsqueda implementada, falta tipar??

--- src/main.ts
+++ src/main.ts
@@ -51,8 +51,15 @@ if (botonDisminuir) {
 
 // Asocio el valor introducido al botón Buscar
 const botonBuscar = document.getElementById("botonBuscar");
-    if (botonBuscar) {
-        botonBuscar.addEventListener("click", buscar)
-    }
 
-
+if (botonBuscar) {
+    botonBuscar.addEventListener("click", function(event: MouseEvent) {
+        const barraBuscar = document.getElementById("barraBuscar") as HTMLInputElement;
+        const numeroIntroducido: number = parseInt(barraBuscar.value);
+        
+        if (!isNaN(numeroIntroducido) && numeroIntroducido >=0 && numeroIntroducido <100) {
+            contadorTurno = numeroIntroducido;
+            mostrarContador();
+        }
+    });
+}
